386b3ad23fb4d49403d7fe920ac8761e0ebd4de0
rdaley2 <rdaley2@u.rochester.edu>
2017-09-17 21:31:20 -0400

    CSS Add
    Added CSS + Nav files, added stylesheet links, but body content into .container,

--- MKeane.php
+++ MKeane.php
@@ -3,56 +3,60 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Michael Keane</title> 
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<link rel="stylesheet" type="text/css" href="css/nav.css">
 	</head>
 
     <body>
-        <header>
-			<nav>
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="RDaley.php">Ryan Daley</a></li>
-					<li><a href="MKeane.php">Michael Keane</a></li>
-					<li><a href="KWalsh.php">Kevin Walsh</a></li> 
-				</ul>
-			</nav>
-			<h1>Michael Keane</h1>
-		</header>
-
-        <article>
-        	<h2>Hello!</h2>
-        		<p>I'm a junior at the University of Rochester double majoring in Digital Media Studies and Studio Art. Currently I am pursuing graphic design, my primary interests being in print design, branding, and illustration. </p>
-
-        	<h2>About</h2>
-        		<p>I was born and raised in Rockville, MD, a suburb right outside Washington, DC. I have a brother named Matthew, and an Australian Shepherd named Riley. Outside of my majors, I have also taken courses in computer science, biology, and philosophy, each of which I'm very interested in. I love Hip-Hop, the Marvel Cinematic Universe, nature documentaries, and long trips to the zoo.</p>
-
-			<h2>Skills</h2>
-				<h3>Adobe Suite</h3>
+	    <div class="container">
+	        <header>
+				<nav>
 					<ul>
-						<li>Illustrator</li>
-						<li>Photoshop</li>
-						<li>InDesign</li>
-						<li>Premiere</li>
-						<li>Experience Design</li>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="RDaley.php">Ryan Daley</a></li>
+						<li><a class="current" href="Mclass="current" Keane.php">Michael Keane</a></li>
+						<li><a href="KWalsh.php">Kevin Walsh</a></li> 
 					</ul>
-				<h3>Web Development</h3>
-					<ul>
-						<li>HTML</li>
-						<li>CSS</li>
-						<li>JavaScript</li>
-					</ul>
-				<h3>Programming</h3>
-					<ul>
-						<li>Java</li>
-						<li>Python</li>
-					</ul>
+				</nav>
+				<h1>Michael Keane</h1>
+			</header>
+
+	        <article>
+	        	<h2>Hello!</h2>
+	        		<p>I'm a junior at the University of Rochester double majoring in Digital Media Studies and Studio Art. Currently I am pursuing graphic design, my primary interests being in print design, branding, and illustration. </p>
+
+	        	<h2>About</h2>
+	        		<p>I was born and raised in Rockville, MD, a suburb right outside Washington, DC. I have a brother named Matthew, and an Australian Shepherd named Riley. Outside of my majors, I have also taken courses in computer science, biology, and philosophy, each of which I'm very interested in. I love Hip-Hop, the Marvel Cinematic Universe, nature documentaries, and long trips to the zoo.</p>
+
+				<h2>Skills</h2>
+					<h3>Adobe Suite</h3>
+						<ul>
+							<li>Illustrator</li>
+							<li>Photoshop</li>
+							<li>InDesign</li>
+							<li>Premiere</li>
+							<li>Experience Design</li>
+						</ul>
+					<h3>Web Development</h3>
+						<ul>
+							<li>HTML</li>
+							<li>CSS</li>
+							<li>JavaScript</li>
+						</ul>
+					<h3>Programming</h3>
+						<ul>
+							<li>Java</li>
+							<li>Python</li>
+						</ul>
 
 
-			<h2>Portfolio</h2>
-        		<p>Sorry! My portfolio is currently under construction! Updates will be posted in the coming weeks! While you wait, you can find my Instagram <a href= https://www.instagram.com/keane.gd/>here</a>, where I uploaded some of my work over the summer. </p>
+				<h2>Portfolio</h2>
+	        		<p>Sorry! My portfolio is currently under construction! Updates will be posted in the coming weeks! While you wait, you can find my Instagram <a href= https://www.instagram.com/keane.gd/>here</a>, where I uploaded some of my work over the summer. </p>
 
-			<h2>Contact</h2>
-				<p>For any questions or inquiries, feel free to reach out to me at mkeane@u.rochester.edu or 301-204-5000!</p>
+				<h2>Contact</h2>
+					<p>For any questions or inquiries, feel free to reach out to me at mkeane@u.rochester.edu or 301-204-5000!</p>
 
-		</article>
+			</article>
+		</div>
     </body>
 </html>

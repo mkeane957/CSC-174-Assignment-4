2d433e32f0e178508aeea9fb9d9c7fc2d5fa5d9d
Kevin Walsh <kevinwalsh322@gmail.com>
2017-09-17 22:07:01 -0400

    fixed MKeane.php - had a messed up link in header

--- MKeane.php
+++ MKeane.php
@@ -14,7 +14,7 @@
 					<ul>
 						<li><a href="index.php">Home</a></li>
 						<li><a href="RDaley.php">Ryan Daley</a></li>
-						<li><a class="current" href="Mclass="current" Keane.php">Michael Keane</a></li>
+						<li><a href="MKeane.php">Michael Keane</a></li>
 						<li><a href="KWalsh.php">Kevin Walsh</a></li> 
 					</ul>
 				</nav>
